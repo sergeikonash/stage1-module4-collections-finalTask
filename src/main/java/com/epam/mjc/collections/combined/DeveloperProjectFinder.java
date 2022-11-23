c45b1f686570f16207049cea8c8e0ebac970cd32
sergeikonash <38048905+sergeikonash@users.noreply.github.com>
2022-11-23 12:42:20 +0100

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -20,14 +20,14 @@ public class DeveloperProjectFinder {
 
         @Override
         public int compare(String o1, String o2) {
-            if (o1.length() - o2.length() != 0) {
-                return o1.length() - o2.length();
+            if (o2.length() - o1.length() != 0) {
+                return o2.length() - o1.length();
             } else {
                 List<String> list = new ArrayList<>();
                 list.add(o1);
                 list.add(o2);
                 list.sort(Comparator.reverseOrder());
-                if (o1.equals(list.get(0))) {
+                if (o2.equals(list.get(0))) {
                     return 1;
                 } else {
                     return -1;
